c3bc2f4b6560e8010ab18e9702d7deafa218f64c
Iggi <i.i.kramarenko@gmail.com>
2022-09-15 17:20:22 +0200

    lesson draft

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import requests
 def print_hi(name):
-    print(f'Hi, {name}')
+	# print(f'Hi, {name}')
+	# print('\U0001f621')
+	# str = u'\u005c\u0055\u0030\u0030\u0030\u0031\u0066\u0036\u0032\u0031'
+
+	#a = requests.get('https://dane.gov.pl/datasets')
+	#print(a.text)
+	# https://www.gov.pl/web/kas/komunikaty
+	
+	#b = bytes.fromhex('68').decode('utf-8')
+	#print(chr(int(f'{a}{b}', 16)))
+	# pseudo kod do programu
+	print()
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+	print_hi('PyCharm')
