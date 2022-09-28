521c0d888e2d65cd05ee63f21d2a922b86bc50f5
Iggi <i.i.kramarenko@gmail.com>
2022-09-28 18:18:54 +0200

    fix: added exit from app

--- task_calculator/main.py
+++ task_calculator/main.py
@@ -2,9 +2,12 @@ from calculator import Calculator as Calc
 
 
 def main():
-	print("Wpisz dane w formacie:\nliczba_1 spacja działanie(+, -, *, /) spacja liczba_2\n")
+	print("Wpisz dane w formacie:\nliczba_1 spacja działanie [+ - * /] spacja liczba_2\n albo nic aby wyjść:")
 	while True:
 		data = input()
+		if data == '':
+			print('=(')
+			exit()
 		result = Calc.run(data)
 		if result == '':
 			print("Błąd w danych, spóbój ponownie...")
@@ -12,8 +15,7 @@ def main():
 		elif result == ZeroDivisionError:
 			print("Nie można dzielić przez zero!")
 			continue
-		print(f'Wynik: {result}')
-		break
+		print(f'Wynik: {result}\ndawaj jeszcze...')
 
 
 if __name__ == '__main__':
